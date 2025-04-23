8671db58301140158417dae4c0857f19c200b8b8
polarccc <lichen2022@stu.hit.edu.cn>
2025-04-23 11:07:05 +0800

    修改注释

--- Lab2/code/semantic.h
+++ Lab2/code/semantic.h
@@ -17,14 +17,11 @@ typedef struct table* pTable;
 typedef struct type {
     Kind kind; // 类型种类（基本类型、数组、结构体、函数）
     union {
-        // 基本类型
         BasicType basic;
-        // 数组类型信息包括元素类型与数组大小构成
         struct {
             pType elem;
             int size;
         } array;
-        // 结构体类型信息是一个链表
         struct {
             char* structName;
             pFieldList field;
